1cf382ceedd5bf76fbf285b651f7fc874f5ef885
aaolive <59589828+aaolive@users.noreply.github.com>
2025-04-05 20:55:12 +0000

    Resposta para contruindo consultas para: ativade 2 e 3.

--- exercicios/2-construindo-consultas/3-condicional.sql
+++ exercicios/2-construindo-consultas/3-condicional.sql
@@ -1,2 +1,8 @@
 -- Escreva uma consulta que retorne o nome por extenso dos estados brasileiros
-
+SELECT DISTINCT Country AS Pais, State AS Sigla_Estado,
+CASE
+  WHEN State = 'SP' THEN 'Sao paulo'
+  WHEN State = 'RJ' THEN 'Rio de Janeiro'
+END AS Estado
+FROM customers
+WHERE Country = 'Brazil';
