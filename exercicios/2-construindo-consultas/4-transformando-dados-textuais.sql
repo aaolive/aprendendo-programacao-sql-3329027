b6df93b08f6af9108fbb8579d897721359012d4f
aaolive <59589828+aaolive@users.noreply.github.com>
2025-04-08 01:13:56 +0000

    Completando contruindo consultas exercicios 2.

--- exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
+++ exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
@@ -1,16 +1,30 @@
 -- Crie uma consulta que retorne as colunas CustomerId, FirstName, LastName e Address das pessoas consumidoras brasileiras
+SELECT CustomerId, FirstName, LastName, Address FROM customers
+WHERE Country LIKE 'Brazil';
 
 
 -- Na consulta anterior, converta o sobrenome para letras maiúsculas
+SELECT CustomerId, FirstName, UPPER(LastName), Address FROM customers
+WHERE Country LIKE 'Brazil';
 
 
 -- Na consulta anterior, converta o nome para letras minúsculas
+SELECT CustomerId, UPPER(FirstName), LastName, Address FROM customers
+WHERE Country LIKE 'Brazil';
 
 
 -- Na consulta anterior, adicione uma coluna cujos os dados seja o resultado da concatenação das colunas de nome e sobrenome
+SELECT CustomerId, CONCAT(FirstName,' ', LastName) AS 'Nome Completo', Address FROM customers
+WHERE Country LIKE 'Brazil';
+
 
 
 -- Na consulta anterior, substitua "Av." por "Avenida" na coluna endereço
+SELECT CustomerId, CONCAT(FirstName,' ', LastName) AS 'Nome Completo', REPLACE(Address, 'Av.', 'Avenida') AS Address FROM customers
+WHERE Country LIKE 'Brazil';
+
 
 
 -- Na consulta anterior, utilize o símbolo de coringa para construir padrões textuais
+SELECT CustomerId, CONCAT(FirstName,' ', LastName) AS 'Nome Completo', REPLACE(Address, 'Av.', 'Avenida') AS Address FROM customers
+WHERE Country LIKE '%azil';
--- exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
+++ exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
@@ -1,1 +1,5 @@
 -- Para a tabela "invoices", crie uma consulta que retorne o país e a soma das compras realizadas nele, bem como o valor da compra mínima e máxima, a quantidade de compras realizadas e o ticket médio de cada compra.
+SELECT BillingCountry, MIN(total), Max(total), Sum(Total), AVG(Total), Count(*)
+FROM invoices
+WHERE BillingCountry LIKE 'Brazil';
+
