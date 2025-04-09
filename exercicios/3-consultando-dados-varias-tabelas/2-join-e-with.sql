3f45b072c76504f57764fbbf128b03e82edb379a
aaolive <59589828+aaolive@users.noreply.github.com>
2025-04-09 00:16:16 +0000

    Resolvendo exercicios 3 - consultando dados varias tabelas.

--- exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
+++ exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
@@ -1,4 +1,13 @@
 -- Crie uma consulta para realizar inner join com as tabelas "tracks", "albums" e "artists"
+SELECT * FROM albums
+LEFT JOIN tracks ON albums.AlbumId = tracks.AlbumId
+LEFT JOIN artists ON albums.ArtistId = artists.ArtistId
+Limit 20;
 
 
 -- Refatore a consulta anterior usando a cláusula "with" e verifique o total de músicas existente na base de terminado artista, ex: Caetano Veloso
+SELECT COUNT(*) AS 'Quantidade musicas para artista' FROM albums
+LEFT JOIN tracks ON albums.AlbumId = tracks.AlbumId
+LEFT JOIN artists ON albums.ArtistId = artists.ArtistId
+WHERE tracks.composer LIKE '%Caetano%';
+-- OBS: rever com with
